94006e9ff3c066f06fcca22d207ad20356ae7bf9
EgorBo <egorbo@gmail.com>
2020-04-02 11:29:39 +0300

    Fix arm64

--- src/mono/ios/appbuilder/appbuilder.cs
+++ src/mono/ios/appbuilder/appbuilder.cs
@@ -13,9 +13,25 @@ public class Program
         string outputFile = args[1];
         string[] objFiles = Directory.GetFiles(inputFolder, "*.dll.o");
 
+        //  Generate 'modules.m' in order to register all managed libraries
+        //
+        //
+        // extern void *mono_aot_module_Lib1_info;
+        // extern void *mono_aot_module_Lib2_info;
+        // ...
+        //
+        // void mono_ios_register_modules (void)
+        // {
+        //     mono_aot_register_module (mono_aot_module_Lib1_info);
+        //     mono_aot_register_module (mono_aot_module_Lib2_info);
+        //     ...
+        // }
+
         var lsDecl = new StringBuilder();
         lsDecl
             .AppendLine("#include <mono/jit/jit.h>")
+            .AppendLine();
+            .AppendLine("#ifdef DEVICE");
             .AppendLine();
 
         var lsUsage = new StringBuilder();
@@ -33,7 +49,14 @@ public class Program
             lsDecl.Append("extern void *").Append(symbol).Append(';').AppendLine();
             lsUsage.Append("\tmono_aot_register_module (").Append(symbol).Append(");").AppendLine();
         }
-        lsDecl.AppendLine().Append(lsUsage).AppendLine("}").AppendLine();
+        lsDecl
+            .AppendLine()
+            .Append(lsUsage)
+            .AppendLine("}")
+            .AppendLine()
+            .AppendLine("#endif")
+            .AppendLine();
+
         File.WriteAllText(outputFile, lsDecl.ToString());
     }
 }
